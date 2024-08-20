8f4460c72f7a6731ed7c348a814e9748599c84c0
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-20 21:29:26 +0900

    Title: 장학금, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/장학금/scholarship.py
+++ 240820/장학금/scholarship.py
@@ -1,7 +1,7 @@
 score1, score2 = map(int, input().split())
 if score1 >= 90 and score2 >= 95:
-    print(10)
+    print(100000)
 elif score1 >= 90 and score2 >= 90:
-    print(5)
+    print(50000)
 else:
     print(0)
