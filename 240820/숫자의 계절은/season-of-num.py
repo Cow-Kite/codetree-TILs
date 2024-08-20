47962c07d3c3377dacbaec4932c817fad4a85102
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-20 21:25:05 +0900

    Title: 숫자의 계절은, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240820/숫자의 계절은/season-of-num.py
@@ -0,0 +1,12 @@
+month = int(input())
+
+if month >= 12:
+    print('Winter')
+elif month >= 9:
+    print('Fall')
+elif month >= 6:
+    print('Summer')
+elif month >= 3: 
+    print('Spring')
+else:
+    print('Winter')
