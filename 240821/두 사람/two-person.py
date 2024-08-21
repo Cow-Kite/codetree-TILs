3bd04a668c7301bfff67851c8469f504a58ea55b
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-21 11:16:16 +0900

    Title: 두 사람, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240821/두 사람/two-person.py
+++ 240821/두 사람/two-person.py
@@ -4,7 +4,9 @@ age2, sex2 = input().split(" ")
 age1 = int(age1)
 age2 = int(age2)
 
-if (age1 >= 19 or age2 >= 19) and (sex1 == 'M' or sex2 == 'M'):
+if age1 >= 19 and sex1 == 'M':
+    print(1)
+elif age2 >= 19 and sex2 == 'M':
     print(1)
 else:
     print(0)
