a4fd76abfb2534be61f1c773fcfb0fe51e38f291
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-21 22:47:02 +0900

    Title: 청소당번 정하기, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240821/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,14 @@
+cnt_2 = 0
+cnt_3 = 0 
+cnt_12 = 0
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 12 == 0:
+        cnt_12 += 1
+    elif i % 3 == 0:
+        cnt_3 += 1
+    elif i % 2 == 0:
+        cnt_2 += 1
+
+print(f'{cnt_2} {cnt_3} {cnt_12}')
