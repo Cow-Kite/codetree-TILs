8d111ce3ab48b1a6765ba4c7e9d4f2ae590d4451
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-23 19:46:57 +0900

    Title: 10개 입력받기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/10개 입력받기/receive-10-inputs.py
+++ 240823/10개 입력받기/receive-10-inputs.py
@@ -1,11 +1,10 @@
 arr = list(map(int, input().split()))
 cnt = 0
+hap = 0
 for i in arr:
     if i == 0:
         break
+    hap += i
     cnt += 1
-hap = 0
-for i in range(0, cnt):
-    hap += arr[i]
 
 print(f'{hap} {hap/cnt:.1f}')
