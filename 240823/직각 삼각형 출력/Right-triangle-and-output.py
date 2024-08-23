e1efed7669300a203240ee233702bc8ea8f45cfd
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-23 16:37:34 +0900

    Title: 직각 삼각형 출력, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240823/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,4 +1,8 @@
+# 변수 선언 및 입력
 n = int(input())
 
-for i in range(0, n):
-    print('*'*(2*i+1))
+# 길이가 n인 직각삼각형을 출력합니다.
+for i in range(n):
+	for _ in range(2 * i + 1):
+		print("*", end="")
+	print()
