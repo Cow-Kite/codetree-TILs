b76bc858a888767e6ca03bc44d810435d36dae2a
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-24 02:05:37 +0900

    Title: 숫자 출력하기 4, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240824/숫자 출력하기 4/print-number-rectangle-4.py
+++ 240824/숫자 출력하기 4/print-number-rectangle-4.py
@@ -7,11 +7,12 @@ if k == 2:
     for i in range(1, n+1):
         if i % 2 == 0:
             for j in range(1, n+1):
-                print(f'{j} ')
+                print(f'{j} ', end=" ")
+            print()
         else:
             for h in range(n, 0, -1):
-                print(f'{h} ')
-
+                print(f'{h} ', end=" ")
+            print()
 if k == 3:
     for i in range(1, n+1):
         for j in range(i, i+n+1):
