baf86a527571b26ffc7645789119262616f7239f
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-23 17:35:04 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240823/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split(" ")))
+
+hap = 0
+cnt = 0
+for num in arr[:]:
+    if num >= 250:
+        break
+    hap += num    
+    cnt += 1
+
+print(f'{hap} {hap/cnt:.1f}')
