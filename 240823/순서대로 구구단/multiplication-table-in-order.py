67b7ce9ae2d2c1b5ca4247d70554f447ba13da98
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-23 21:42:20 +0900

    Title: 순서대로 구구단, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240823/순서대로 구구단/multiplication-table-in-order.py
+++ 240823/순서대로 구구단/multiplication-table-in-order.py
@@ -6,6 +6,6 @@ if a <= b:
         print()
 else:
     for i in range(1, 10):
-        for j in range(b, a-1, -1):
+        for j in range(a, b-1, -1):
             print(f'{j} * {i} = {i * j}', end = "  ")
         print()
