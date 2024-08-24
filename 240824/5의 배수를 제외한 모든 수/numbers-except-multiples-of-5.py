e20306b26130df203c89be8bc381006e5641360b
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-24 22:01:37 +0900

    Title: 5의 배수를 제외한 모든 수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240824/5의 배수를 제외한 모든 수/numbers-except-multiples-of-5.py
+++ 240824/5의 배수를 제외한 모든 수/numbers-except-multiples-of-5.py
@@ -3,11 +3,11 @@ x, y = map(int, input().split())
 hap = 0 
 cnt = 0
 
-for i in range(x, y+1):
-    if i % 5 == 0:
-        continue
-    hap += i
-    cnt += 1
+max_num = max(x, y)
+min_num = min(x, y)
+for i in range(min_num, max_num+1):
+    if i % 5 != 0:
+        hap += i
+        cnt += 1
 
-if cnt =
 print(f'{hap} {hap/cnt:.1f}')
