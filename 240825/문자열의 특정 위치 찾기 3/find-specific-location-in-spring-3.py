3a358a5e2de644aa5435a37c591e9273043ca459
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-25 20:19:48 +0900

    Title: 문자열의 특정 위치 찾기 3, Time: 209ms, Memory: 24MB, Status: Passed - CodeTree

--- 240825/문자열의 특정 위치 찾기 3/find-specific-location-in-spring-3.py
+++ 240825/문자열의 특정 위치 찾기 3/find-specific-location-in-spring-3.py
@@ -1,11 +1,11 @@
 str1 = list(input())
 c = input()
-cnt = 1
+cnt = 1 
 for char in str1:
     if char == c:
         break
     cnt += 1
-if cnt == len(str1)-1:
+if cnt > len(str1):
     print('Not Found')
 else:
     print(cnt)
