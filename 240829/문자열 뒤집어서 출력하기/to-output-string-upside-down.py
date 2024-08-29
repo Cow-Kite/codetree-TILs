fa2fd0dd9cb99269a4d03106b8f68eb3b9aa807c
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-29 21:22:35 +0900

    Title: 문자열 뒤집어서 출력하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240829/문자열 뒤집어서 출력하기/to-output-string-upside-down.py
+++ 240829/문자열 뒤집어서 출력하기/to-output-string-upside-down.py
@@ -1,8 +1,6 @@
 string = []
 for _ in range(4):
     string.append(input())
-    
-for i in range(len(string)-1, -1, -1):
-    for j in range(len(string[i])-1, -1, -1):
-        print(string[i][j], end="")
-    print()
+
+for elem in string[::-1]:
+    print(elem[::-1])
