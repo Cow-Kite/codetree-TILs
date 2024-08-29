990dd9e89e7eb3b9704f85cc45c9c53baa2e3a6d
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-30 00:06:11 +0900

    Title: 마지막으로 끝난 문자, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240830/마지막으로 끝난 문자/the-last-text.py
@@ -0,0 +1,16 @@
+n = int(input())
+string = []
+for _ in range(n):
+    string.append(input())
+
+A = input()
+dap = []
+cnt = 0
+for elem in string:
+    if elem[-1] == A:
+        cnt += 1
+        dap.append(elem)
+
+print(cnt)
+for elem in dap:
+    print(elem)
