42628347a40044e1e2090f4d6e4c22c4e077d79b
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-08-30 21:26:29 +0900

    Title: 학생들의 점수대 파악하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240830/학생들의 점수대 파악하기/understand-students-scores.py
+++ 240830/학생들의 점수대 파악하기/understand-students-scores.py
@@ -6,6 +6,6 @@ score = list(map(int, input().split()))
 for i in score:
     arr[i // 10] += 1
 
-for i in range(len(arr)-1, -1, -1):
+for i in range(len(arr)-1, 0, -1):
     if arr[i] != 0:
         print(f'{i*10} - {arr[i]}')
