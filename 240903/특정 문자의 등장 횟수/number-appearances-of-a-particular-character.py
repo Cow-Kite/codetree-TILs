a6652f5e657824b894730d5a626b2f6ebe4db3ee
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-09-03 15:02:14 +0900

    Title: 특정 문자의 등장 횟수, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240903/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,4 +1,10 @@
 string = input()
+ee = 0
+eb = 0
+for i in range(len(string)-1):
+    if string[i:i+2] == 'ee':
+        ee += 1
+    if string[i:i+2] == 'eb':
+        eb += 1
 
-print(string.count('ee'), end=" ")
-print(string.count('eb'))
+print(ee, eb)
