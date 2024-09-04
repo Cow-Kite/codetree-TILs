885752f9e9987c6cc6362b4f54fe0019ba8097a2
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2024-09-04 20:40:49 +0900

    Title: -999, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240904/-999/-999.py
+++ 240904/-999/-999.py
@@ -1,6 +1,10 @@
 arr = list(map(int, input().split()))
 
-max_num = max(arr[:-1])
-min_num = min(arr[:-1])
+if len(arr) == 100:
+    max_num = max(arr)
+    min_num = min(arr)
+else:
+    max_num = max(arr[:-1])
+    min_num = min(arr[:-1])
 
 print(max_num, min_num)
